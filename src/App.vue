841d9915843d8dbe47434bdb8393b013ed561617
jblavisse <jean-baptiste@jblavisse.fr>
2021-07-08 11:22:04 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2 class="title">{{msg}} {{score}}</h2>
+
+    <input type="text" v-model="firstname">
+    {{firstname}}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+  },
+  data: function() {
+    return {
+      msg: "Prout Prout!",
+      firstname: "",
+      score: 0
+    }
+  } 
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .title {
+    color: red;
+  }
 </style>
